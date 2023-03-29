8508cd8c78fec98f66b541f5a6c7ba556761d600
Domantas Petrauskas <domantas@zyro.com>
2023-03-29 17:25:11 +0300

    Disable legacy bundling

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
-import legacy from '@vitejs/plugin-legacy'
+// import legacy from '@vitejs/plugin-legacy'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), legacy({
-    targets: ['defaults, safari > 13']
-  })],
+  plugins: [
+    vue(),
+    // legacy({
+    //   targets: ['defaults, safari > 13']
+    // })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
