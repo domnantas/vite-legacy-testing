1bc626415eb2a10764d586698704c2c4136abf6a
Domantas Petrauskas <domantas@zyro.com>
2023-03-29 17:24:28 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const numbers = ref([0, 1, 2, 3]);
+const lastNumber = computed(() => numbers.value.at(-1));
+</script>
+
+<template>
+  {{ lastNumber }}
+</template>
+
